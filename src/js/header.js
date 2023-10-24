c51bd2748f37f4a1cb63e407204386f19924338d
Maks3502 <maks1234promiron18@gmail.com>
2023-10-24 23:22:02 +0300

    header

---
+++ src/js/header.js
@@ -0,0 +1,14 @@
+const headerBtn = document.querySelector(".button_header")
+
+function headerModal() {
+    document.getElementById("myDropdown").classList.toggle("show");
+};
+
+window.onclick = function(e) {
+    if (!e.target.matches(headerBtn)) {
+      var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+          myDropdown.classList.remove('show');
+        }
+    }
+  }
