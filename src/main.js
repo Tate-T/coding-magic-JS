34328b2ba689812e3579e786d0ceb6d328a9ceb7
Oleg Bondarenko <109847185+route333@users.noreply.github.com>
2023-10-25 20:49:58 +0300

    computer-num-game full

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
+import './js/header';
 import './js/year-game';
+import './js/computer-num-game';
+import './js/google-dino';
 import './js/football';
 import './js/3-nums-game';
-import './js/google-dino';
-import './js/header';
