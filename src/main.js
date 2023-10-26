554ba747d099044d6436ca309c4e7d293a6c21d2
diana7770 <20diane09@gmail.com>
2023-10-27 00:03:13 +0300

    time-calculator

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import './js/year-game';
-import './js/football';
-import './js/3-nums-game';
-import './js/google-dino';
+import "./js/year-game";
+import "./js/football";
+import "./js/3-nums-game";
+import "./js/time-calculator";
+import "./js/google-dino";
