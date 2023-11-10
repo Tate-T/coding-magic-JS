a1b5b7544c8ff19e61dd62706b208d97603a69e7
Maks3502 <maks1234promiron18@gmail.com>
2023-11-10 22:28:27 +0200

    structur

--- src/js/choose-genious.js
+++ src/js/choose-genious.js
@@ -1,12 +1,5 @@
 
-function headerModal() {
-    var dropdownContent = document.getElementById("myDropdown");
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
-  }
+
   
   // header
   // main
