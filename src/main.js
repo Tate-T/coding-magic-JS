8de9135056ee90dddd6d21c6ac633adb1866d169
diana7770 <20diane09@gmail.com>
2023-11-11 11:55:29 +0200

    our-team1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "./js/year-game";
 import "./js/computer-num-game";
 import "./js/calculator";
+import "./js/our-team";
 import "./js/football";
 import "./js/3-nums-game";
 import "./js/time-calculator";
