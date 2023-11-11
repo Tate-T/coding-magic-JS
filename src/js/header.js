6892227a20e3f6acac8bfc242a623c72ecc71726
Maks3502 <maks1234promiron18@gmail.com>
2023-11-11 13:18:44 +0200

    formheader

--- src/js/header.js
+++ src/js/header.js
@@ -7,3 +7,37 @@ function headerModal() {
     dropdownContent.style.display = "block";
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var form = document.querySelector('.form_header');
+  var closeButton = document.querySelector('.button_close_btn');
+  var saveButton = document.querySelector('.btn_saveAndClose');
+  var backdrop = document.querySelector('.backdrop_header'); // Add this line
+
+
+  function openForm() {
+      console.log('Opening form');
+      form.style.transform = 'scale(1)';
+      backdrop.style.display = 'block'; 
+  }
+
+
+  function closeForm() {
+      console.log('Closing form');
+      form.style.transform = 'scale(1.1)';
+      backdrop.style.display = 'none'; 
+  }
+
+  function saveAndClose(event) {
+      event.preventDefault(); 
+      console.log('Saving and closing form');
+      
+      closeForm();
+  }
+
+ 
+  closeButton.addEventListener('click', closeForm);
+  saveButton.addEventListener('click', saveAndClose);
+
+ 
+});
